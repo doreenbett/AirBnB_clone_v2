cfb69ecf25b7931694a53a3f737467fb5696961f
doreenbett <doreenbett001@gmail.com>
2022-07-12 03:48:16 -0700

    Modified code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,25 +5,24 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
--- Creates a MySQL server with:
-	--   Database hbnb_test_db.
-	  --   User hbnb_test with password hbnb_test_pwd in localhost.
-	  --   Grants all privileges for hbnb_test on hbnb_test_db.
-	  --   Grants SELECT privilege for hbnb_test on performance_schema.
+--   Creates a MySQL server with:
+--   Database hbnb_test_db.
+--   User hbnb_test with password hbnb_test_pwd in localhost.
+--   Grants all privileges for hbnb_test on hbnb_test_db.
+--   Grants SELECT privilege for hbnb_test on performance_schema.
 	  
-	  CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-	  CREATE USER
-	      IF NOT EXISTS 'hbnb_test'@'localhost'
-	          IDENTIFIED BY 'hbnb_test_pwd';
-		  GRANT ALL PRIVILEGES
-		     ON `hbnb_test_db`.*
-		        TO 'hbnb_test'@'localhost'
-			   IDENTIFIED BY 'hbnb_test_pwd';
-			   GRANT SELECT
-			      ON `performance_schema`.*
-			         TO 'hbnb_test'@'localhost'
-				    IDENTIFIED BY 'hbnb_test_pwd';
-				    FLUSH PRIVILEGES;__file_path = 'file.json'
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER
+IF NOT EXISTS 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';GRANT ALL PRIVILEGES
+ON `hbnb_test_db`.*
+TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT
+ON `performance_schema`.*
+TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;__file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
@@ -72,3 +71,7 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
