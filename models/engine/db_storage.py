1e89944f767b1d2a455ff6258d930899e5b6ac1c
doreenbett <88307192+doreenbett@users.noreply.github.com>
2022-07-13 23:29:22 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -17,7 +17,6 @@ from sqlalchemy.orm import sessionmaker
 
 class DBStorage:
     """Represents a database storage engine.
-
     Attributes:
         __engine (sqlalchemy.Engine): The working SQLAlchemy engine.
         __session (sqlalchemy.Session): The working SQLAlchemy session.
